7b42a6254fad2cfdbb4f8221f84c00ce88099d7a
Doga Ceren Erkek <dogacerenerkek@gmail.com>
2026-03-15 18:53:16 +0100

    first commit

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    // burayı nasıl yapacağımı bilmiyorum
+  }
+}
+
+// Test
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+// ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+// ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
+// ["Nanitoids", "Antigravitator", "Droid"]
